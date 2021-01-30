c59b7f68cf81a7c2bfb5718d1e67e3c855515e89
Dhaval Savalia <hello@dhavalsavalia.com>
2021-01-30 14:26:39 +0530

    Minimal Working Prototype

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
+from typing import Optional
+
+
 from fastapi import FastAPI, status
 from pydantic import BaseModel
 
 
-class Post(BaseModel):
+class PostIn(BaseModel):
     title: str
     body: str
 
@@ -10,18 +13,20 @@ class Post(BaseModel):
 app = FastAPI()
 
 
+posts_db: list = list()
+
+
 @app.get("/posts")
 async def posts():
-    return {"posts": ["First Post"]}
+    return posts_db
 
 
 @app.get("/posts/{post_id}")
 async def read_post(post_id: int):
-    return {"post_id": post_id}
+    return posts_db[post_id]
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_post(post: Post):
+async def create_post(post: PostIn):
+    posts_db.append(post)
     return post
-
-
